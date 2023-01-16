d91f40f63df28b22d27b3ddc2c76437992eeacbc
Masia <kindofmydream@gmail.com>
2023-01-16 15:55:09 +0100

    login page add

--- Front/lib/screens/login_screen.dart
+++ Front/lib/screens/login_screen.dart
@@ -17,55 +17,104 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          image: DecorationImage(
-        image: AssetImage('assets/roslinka.jpg'),
-        fit: BoxFit.cover,
-      )),
-      child: AlertDialog(
-        content: Form(
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Form(
           key: _formKey,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              TextFormField(
-                validator: (input) {
-                  if (input!.isEmpty) {
-                    return 'Please enter login';
-                  }
-                  return null;
-                },
-                onSaved: (input) => _login = input!,
-                decoration: InputDecoration(labelText: 'Login'),
-              ),
-              TextFormField(
-                validator: (input) {
-                  if (input!.isEmpty) {
-                    return 'Please enter a password';
-                  }
-                  return null;
-                },
-                onSaved: (input) => _password = input!,
-                decoration: InputDecoration(labelText: 'Password'),
-                obscureText: true,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    _submit(_login, _password);
-                  }
-                },
-                child: Text('Login'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  _navigateToRegistration();
-                },
-                child: Text('Don\'t have an account? Register'),
-              ),
-            ],
+          child: SafeArea(
+            child: Column(
+              //mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('assets/start_plant.jpg'),
+                ),
+                Text(
+                  'ᑭᒪᗩᑎTITY',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.white,
+                  ),
+                ),
+                Text('TAKE CARE OF PLANTS EASILY',
+                    style: TextStyle(
+                      fontFamily: 'SourceSans3',
+                      color: Colors.black,
+                      fontSize: 15.0,
+                      letterSpacing: 1.5,
+                      fontWeight: FontWeight.bold,
+                    )),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: TextFormField(
+                    validator: (input) {
+                      if (input!.isEmpty) {
+                        return 'Please enter login';
+                      }
+                      return null;
+                    },
+                    onSaved: (input) => _login = input!,
+                    decoration: InputDecoration(
+                        labelText: '   Login',
+                        prefixIcon: Padding(
+                          padding: EdgeInsets.all(0.0),
+                          child: Icon(
+                            Icons.login,
+                            color: Colors.teal
+                          )
+                        )),
+                ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: TextFormField(
+                    style: TextStyle(
+                      decorationColor: Colors.teal,
+                    ),
+                    validator: (input) {
+                      if (input!.isEmpty) {
+                        return 'Please enter a password';
+                      }
+                      return null;
+                    },
+                    onSaved: (input) => _password = input!,
+                    decoration: InputDecoration(
+                        labelText: '   Password',
+                        prefixIcon: Padding(
+                        padding: EdgeInsets.all(0.0),
+                      child: Icon(
+                          Icons.password,
+                          color: Colors.teal
+                      )
+                  )),
+                    obscureText: true,
+                  ),
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll<Color>(Colors.black54),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                      _submit(_login, _password);
+                    }
+                  },
+                  child: Text('Login'),
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll<Color>(Colors.black54),
+                  ),
+                  onPressed: () {
+                    _navigateToRegistration();
+                  },
+                  child: Text('Don\'t have an account? Register'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -101,8 +150,8 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   void _submit(String login, String password) async {
-    _navigateToUser();
-    return;
+    //_navigateToUser();
+    //return;
     var url = 'http://10.0.2.2:5000//login';
     var response = await http.post(
       Uri.parse(url),
